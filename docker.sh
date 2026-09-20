c7cef4cc1fe43db3f0fbe0434ee0f02f72447049
DevendraSinghRathore2693 <devendrasinghrathore2693@gmail.com>
2026-09-20 12:20:27 +0530

    Add Docker installation script for Ubuntu
    This script installs Docker on Ubuntu by adding the official GPG key and repository, updating packages, and installing the latest Docker version.

---
+++ docker.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+####################
+#This is docker installation files
+#Author: Devendra
+#Date: 20th Sept 2026
+###################
+
+set -x
+set -e
+set -o pipefail
+
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+#update packages
+sudo apt update
+
+#install latest docker version
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt update
+
+#check status of docker
+sudo systemctl status docker
